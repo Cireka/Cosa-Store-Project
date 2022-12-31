2b809f3c898b6429d193ded59e8f5045b5107e77
Cireka <tsifekide@gmail.com>
2022-12-31 17:13:19 +0400

    Fixed Fonts on About Page

--- Components/About/About.js
+++ Components/About/About.js
@@ -27,17 +27,21 @@ const AboutComp = () => {
               ipsum eget, dictum enim. Donec non neque ut enim dapibus tincidunt
               vitae nec augue. Suspendisse potenti. Proin ut est diam. Donec
               condimentum euismod tortor, eget facilisis diam faucibus et. Morbi
-              a tempor elit. Donec gravida lorem elit, quis condimentum ex
-              semper sit amet. Fusce eget ligula magna. Aliquam aliquam
-              imperdiet sodales. Ut fringilla turpis in vehicula vehicula.
-              Pellentesque congue ac orci ut gravida. Aliquam erat volutpat.
-              Donec iaculis lectus a arcu facilisis, eu sodales lectus sagittis.
-              Etiam pellentesque, magna vel dictum rutrum, neque justo eleifend
-              elit, vel tincidunt erat arcu ut sem. Sed rutrum, turpis ut
-              commodo efficitur, quam velit convallis ipsum, et maximus enim
-              ligula ac ligula. Any questions? Let us know in store at 8th
-              floor, 379 Hudson St, New York, NY 10018 or call us on (+1) 96 716
-              6879
+              a tempor elit.
+            </p>
+            <p>
+              Donec gravida lorem elit, quis condimentum ex semper sit amet.
+              Fusce eget ligula magna. Aliquam aliquam imperdiet sodales. Ut
+              fringilla turpis in vehicula vehicula. Pellentesque congue ac orci
+              ut gravida. Aliquam erat volutpat. Donec iaculis lectus a arcu
+              facilisis, eu sodales lectus sagittis. Etiam pellentesque, magna
+              vel dictum rutrum, neque justo eleifend elit, vel tincidunt erat
+              arcu ut sem. Sed rutrum, turpis ut commodo efficitur, quam velit
+              convallis ipsum, et maximus enim ligula ac ligula.
+            </p>
+            <p>
+              Any questions? Let us know in store at 8th floor, 379 Hudson St,
+              New York, NY 10018 or call us on (+1) 96 716 6879
             </p>
           </div>
           <div className={style.imgParrent}>
--- pages/_document.js
+++ pages/_document.js
@@ -44,6 +44,12 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@700&display=swap"
           rel="stylesheet"
         />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap"
+          rel="stylesheet"
+        ></link>
       </Head>
       <body>
         <Main />
